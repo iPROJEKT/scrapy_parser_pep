ee0381f49b8d4fec09e15b16fd815a1c2c8c633e
Sergey <prorok20050@mail.ru>
2023-06-06 16:24:09 +0300

    First rew

---
+++ pep_parse/settings.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+BOT_NAME = 'pep_parse'
+SPIDER_MODULES = ['pep_parse.spiders']
+NEWSPIDER_MODULE = 'pep_parse.spiders'
+ROBOTSTXT_OBEY = True
+ITEM_PIPELINES = {'pep_parse.pipelines.PepParsePipeline': 300}
+FEEDS = {
+    'results/pep_%(time)s.csv': {
+        'format': 'csv',
+        'fields': ['number', 'name', 'status'],
+        'overwrite': True
+    },
+}
+NOW_TIME = datetime.strftime(datetime.now(), '%Y-%m-%dT%H-%M-%S')
+
+FILE_NAME = 'status_summary_'
+RESULT_NAME_FOLDER = 'results'
+FORMAT_FILE = '.csv'
+ALLOWED_DOMAINS = 'peps.python.org'
+START_URLS = 'https://peps.python.org/'
